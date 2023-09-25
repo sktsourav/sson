932848743a3aeb86f3b0fd5de978f8a40be422ee
Sourav <souravkumar.thakur@olrretail.com>
2023-09-25 10:08:46 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("div", {}, 
+    [React.createElement("div", {}, [React.createElement("h1", {}, "Hello from JS Sibling 1"), React.createElement("h1", {}, "Hello from JS Sibling 2")]),
+    React.createElement("div", {}, [React.createElement("h1", {}, "Hello from JS Sibling 3"), React.createElement("h1", {}, "Hello from JS Sibling 4")])]
+    )
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
