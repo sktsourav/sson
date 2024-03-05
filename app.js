6da3b4d562c47db2dec05a82316201aaf6af6f4a
Sourav <souravkumar.thakur@olrretail.com>
2024-03-05 11:00:53 +0530

    Removed cdn links used react using import

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement("div", {}, 
     [React.createElement("div", {}, [React.createElement("h1", {}, "Hello from JS Sibling 1"), React.createElement("h1", {}, "Hello from JS Sibling 2")]),
     React.createElement("div", {}, [React.createElement("h1", {}, "Hello from JS Sibling 3"), React.createElement("h1", {}, "Hello from JS Sibling 4")])]
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
 Jai Ganesh
+
+
+#PARCEL
+- Dev Build
+- Local Server
+- HMR - Hot module replacement
+- File watching algorithm - written in C++
+- caching - faster builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistency Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagonastics
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unsed code
