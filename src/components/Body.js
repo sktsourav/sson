2e1b3ff53ea0a061a0a999cee2705c15ef79679f
Sourav <souravkumar.thakur@olrretail.com>
2024-03-13 18:19:22 +0530

    Added ratting filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import RestaurantsList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+    const [restaurantsList, setRestaurantsList] = useState(RestaurantsList)
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <button className="filter-btn" onClick={() => {
+                const filteredList = RestaurantsList.filter(res => res?.info?.avgRating >= 4)
+                setRestaurantsList(filteredList)
+            }}
+            >
+                Filter by ratting</button>
             <div className="restaurant-container">
                 {
-                    RestaurantsList.map((restaurant) => (
+                    restaurantsList.map((restaurant) => (
                         <RestaurantCard key={restaurant?.info?.id} restaurant={restaurant} />
                     ))
                 }
