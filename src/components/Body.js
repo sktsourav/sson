d24e622d39070ba17f5b57ac651d52b4842094c2
Sourav <souravkumar.thakur@olrretail.com>
2024-03-19 15:41:35 +0530

    Basic search feature developed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,50 @@
-import { useState } from "react"
-import RestaurantsList from "../utils/mockData";
+import { useState, useEffect } from "react"
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { SWIGGY_URL } from "../utils/constants";
 
 const Body = () => {
-    const [restaurantsList, setRestaurantsList] = useState(RestaurantsList)
-    return (
+    const [restaurantList, setRestaurantList] = useState([]);
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        fetchRestaurants();
+    }, [])
+
+    const fetchRestaurants = async () => {
+        const data = await fetch(SWIGGY_URL)
+        const resList = await data.json();
+        setRestaurantList(resList?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
+    const search = () => {
+        const searchList = restaurantList.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+        setRestaurantList(searchList)
+    }
+
+    return restaurantList.length === 0 ? <Shimmer /> : (
         <div className="body">
-            <button className="filter-btn" onClick={() => {
-                const filteredList = RestaurantsList.filter(res => res?.info?.avgRating >= 4)
-                setRestaurantsList(filteredList)
-            }}
-            >
-                Filter by ratting</button>
+            <div className="filter">
+                <div className="search">
+                    <input
+                        type="text"
+                        className="search-box"
+                        value={searchText}
+                        onChange={e => setSearchText(e.target.value)} />
+                    <button onClick={search}>
+                        Search
+                    </button>
+                </div>
+                <button className="filter-btn" onClick={() => {
+                    const filteredList = restaurantList.filter(res => res?.info?.avgRating >= 4.2)
+                    setRestaurantList(filteredList)
+                }}>
+                    Filter by ratting
+                </button>
+            </div>
             <div className="restaurant-container">
                 {
-                    restaurantsList.map((restaurant) => (
+                    restaurantList.map((restaurant) => (
                         <RestaurantCard key={restaurant?.info?.id} restaurant={restaurant} />
                     ))
                 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const CDN_APP_LOGO = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
 export const CDN_RESTAURANT_LOGO = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+export const SWIGGY_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.6692956&lng=86.15111200000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
